8fb0ccfd7ccab1a81678d6b6736e42961a2495ee
Samayun Chowdhury <31636535+samayun@users.noreply.github.com>
2022-10-21 14:18:35 +0600

    feat: volume of cylinder

---
+++ volume.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main () {
+    float radius, hight, vol;
+  
+    printf("Radius number : ");
+    scanf("%f", &radius );
+    printf("Hight number : ");
+    scanf("%f", &hight );
+
+    vol = 3.1416*radius*radius*hight;
+
+    printf("vol is : %.2f",vol);
+    return 0;
+}
