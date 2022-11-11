093b6152f0c0827022833238bb8ea30d7214868b
Samayun Chowdhury <31636535+samayun@users.noreply.github.com>
2022-11-11 13:35:20 +0600

    Create one-hundred.c

---
+++ one-hundred.c
@@ -0,0 +1,14 @@
+/*
+  Author: Samayun Chowdhury
+  Source: https://github.com/samayun/c-programming
+  */
+#include <stdio.h>
+
+int main() {
+    int num = 1;
+    while(num <= 100){
+     printf("%d \t", num);
+     num++;
+  }
+  return 0;
+}
