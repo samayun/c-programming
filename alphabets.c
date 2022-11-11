951e171e2e32f023840e0623452cea2f21698287
Samayun Chowdhury <31636535+samayun@users.noreply.github.com>
2022-11-11 13:25:23 +0600

    feat: a to z print

---
+++ alphabets.c
@@ -0,0 +1,12 @@
+/*
+  Author: Samayun Chowdhury
+  Source: https://github.com/samayun/c-programming
+  */
+#include <stdio.h>
+
+int main() {
+    char c;
+    for (c = 'A'; c <= 'Z'; ++c)
+        printf("%c ", c);
+    return 0;
+}
